0b048d72d0722059a1334bfe6f1e80935e6b7903
Kristian-Krastev <44848168+Kristian-Krastev@users.noreply.github.com>
2019-03-29 09:21:09 +0200

    adding solution

---
+++ Templates/Source.cpp
@@ -0,0 +1,28 @@
+#include "Stack.h"
+
+int main() {
+
+	Stack<int> s;
+	
+	s.push(5);
+	s.push(10);
+	s.push(123);
+	s.push(54);
+	s.push(23);
+
+	std::cout << s.peek() << '\n';
+	s.pop();
+
+	std::cout << s.peek() << '\n';
+	s.pop();
+
+	std::cout << s.peek() << '\n';
+	s.pop();
+
+	std::cout << s.peek() << '\n';
+	s.pop();
+
+	std::cout << s.peek() << '\n';
+	s.pop();
+	return 0;
+}
